197fe63cffc9fba47b278acaffda3b4c644f9c75
Martin Esteban De Los Santos <141642941+medchajari@users.noreply.github.com>
2025-05-13 07:22:57 -0300

    Update pacienteData.java

--- src/Metodos/pacienteData.java
+++ src/Metodos/pacienteData.java
@@ -18,13 +18,13 @@ import org.mariadb.jdbc.Statement;
 public class pacienteData {
     private Conexion con;
     public pacienteData() {
-        con = new Conexion(); // Crear una instancia de la clase Conexion
+        con = new Conexion(); 
         
     }
     
      public void agregarPacientes(String nombre, String apellido, String fecha_nacimiento, String dni, String direccion, String telefono,String email, String genero,int historia_clinica, String observacion, double saldo) {
     try {
-        Connection connection = con.getConexion(); // Obtener la conexión de la instancia de Conexion
+        Connection connection = con.getConexion(); 
         String sql = "INSERT INTO pacientes (nombre,apellido,fecha_nacimiento,dni,direccion,telefono,email,genero,historia_clinica,observacion,saldo) VALUES (?, ?, ?, ?, ?,?, ?,?,?,?,?)";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
         preparedStatement.setString(1, nombre);
@@ -241,54 +241,11 @@ public class pacienteData {
     
     return pacientes;
 }
-//public Paciente buscarPacientePorId(int idPaciente) {
-//    Paciente paciente = null;
-//    try {
-//        Connection connection = con.getConexion(); // Obtener la conexión de la instancia de Conexion
-//        String sql = "SELECT * FROM pacientes WHERE id_paciente = ?";
-//        PreparedStatement preparedStatement = connection.prepareStatement(sql);
-//        preparedStatement.setInt(1, idPaciente);
-//        ResultSet resultSet = preparedStatement.executeQuery();
-//
-//        if (resultSet.next()) {
-//            String nombre = resultSet.getString("nombre");
-//            String apellido = resultSet.getString("apellido");
-//            String fechaNacimiento = resultSet.getString("fecha_nacimiento");
-//            String dni = resultSet.getString("dni");
-//            String direccion = resultSet.getString("direccion");
-//            String telefono = resultSet.getString("telefono");
-//            String email = resultSet.getString("email");
-//            String genero = resultSet.getString("genero");
-//            int historiaClinica = resultSet.getInt("historia_clinica");
-//            String observacion = resultSet.getString("observacion");
-//            double saldo = resultSet.getDouble("saldo");
-//
-//            paciente = new Paciente();
-//            paciente.setIdPaciente(idPaciente);
-//            paciente.setNombre(nombre);
-//            paciente.setApellido(apellido);
-//            paciente.setFechaNacimiento(fechaNacimiento);
-//            paciente.setDni(dni);
-//            paciente.setDireccion(direccion);
-//            paciente.setTelefono(telefono);
-//            paciente.setEmail(email);
-//            paciente.setGenero(genero);
-//            paciente.setHistoriaClinica(historiaClinica);
-//            paciente.setObservacion(observacion);
-//            paciente.setSaldo(saldo);
-//        }
-//
-//        resultSet.close();
-//        preparedStatement.close();
-//    } catch (SQLException e) {
-//        e.printStackTrace();
-//    }
-//    return paciente;
-//}
+
 public Paciente buscarPacientePorId(int idPaciente) {
     Paciente paciente = null;
     try {
-        Connection connection = con.getConexion(); // Obtener la conexión de la instancia de Conexion
+        Connection connection = con.getConexion(); 
         String sql = "SELECT * FROM pacientes WHERE id_paciente = ?";
         PreparedStatement preparedStatement = connection.prepareStatement(sql);
         preparedStatement.setInt(1, idPaciente);
